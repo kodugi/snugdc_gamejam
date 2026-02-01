eb7a0c16bcf870494277b6675e1d6380cfaba241
JunwooPark <76034729+kodugi@users.noreply.github.com>
2026-02-02 01:11:36 +0900

    State Machine Pattern을 이용한 리팩터링 및 GameState 수정

--- Assets/Resources/Data/GameState.cs
+++ Assets/Resources/Data/GameState.cs
@@ -1,11 +1,16 @@
 public enum GameState
 {
     GameStart,
+    StartRound,
     SelectSentence,
+    StartRun,
     TurnStart,
+    WaitForInput,
     PlayItem,
     Interpret,
     TurnEnd,
     RevealAnswer,
+    EndRun,
+    EndRound,
     GameOver
 }
