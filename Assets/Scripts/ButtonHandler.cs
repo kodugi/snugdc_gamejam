ff819641436c69e7b7ad3c271039f59c1ac2a959
류지호 <jihoryu8@gmail.com>
2026-02-01 23:19:20 +0900

    Add highlight effect

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -1,11 +1,15 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ButtonHandler : MonoBehaviour,IPointerClickHandler
 {
+    [SerializeField] Image highlightImage;
     [SerializeField] private TextMeshProUGUI _textMeshPro;
     private Collider2D collider2D;
+    public event Action<int, int,bool> OnButtonClicked;
     
     private string text = "";
     private WordType wordType;
@@ -26,6 +30,8 @@ public class ButtonHandler : MonoBehaviour,IPointerClickHandler
 
     public void Init(WordData wordData,int row,int col)
     {
+        UnHighLight();
+        highlightImage.transform.localScale=Vector3.one*1.3f;
         text=wordData.word;
         SetText(text);
         wordType=wordData.type;
@@ -40,13 +46,25 @@ public class ButtonHandler : MonoBehaviour,IPointerClickHandler
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        onClick();
+        OnClick();
     }
-    public void onClick()
+    void OnClick()
     {
         Debug.Log("clicked");
+        GameManager.Instance.ProcessWordChoice(row,col);
+        OnButtonClicked?.Invoke(row,col,isCorrect);
     }
 
+    public void HighLight()
+    {
+        transform.localScale = Vector3.one * 1.2f;
+        highlightImage.enabled = true;
+    }
+    public void UnHighLight()
+    {
+        transform.localScale = Vector3.one;
+        highlightImage.enabled = false;
+    }
     public void Disablebutton()
     {
      collider2D.enabled = false;
