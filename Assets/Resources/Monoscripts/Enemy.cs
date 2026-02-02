729d1e797a3903ac178f396ec37197efd9a30703
JunwooPark <76034729+kodugi@users.noreply.github.com>
2026-02-02 09:25:11 +0900

    아이템 사용 로직 추가

--- Assets/Resources/Monoscripts/Enemy.cs
+++ Assets/Resources/Monoscripts/Enemy.cs
@@ -22,6 +22,33 @@ public class Enemy: Player
         return new Position(-1, currentColumn);
     }
 
+    public ItemType UseItem()
+    {
+        // 랜덤으로 아이템 사용, 선택하지 않은 경우 None 반환
+        // 실제 아이템 사용 처리는 TurnManager의 PlayItem에서 수행
+        // UseItem이 None을 반환할 때까지 반복 호출, 사용 불가능한 아이템을 선택한 경우 다시 호출
+        List<ItemType> availableItems = new List<ItemType>();
+        foreach(var item in inventory.Keys)
+        {
+            if(inventory[item] > 0)
+            {
+                availableItems.Add(item);
+            }
+        }
+
+        if(availableItems.Count == 0)
+        {
+            return ItemType.None;
+        }
+
+        int randomIndex = UnityEngine.Random.Range(0, availableItems.Count + 1);
+        if (randomIndex == availableItems.Count)
+        {
+            return ItemType.None;
+        }
+        return availableItems[randomIndex];
+    }
+
     public void Initialize(GameManager gameManager)
     {
         _gameManager = gameManager;
