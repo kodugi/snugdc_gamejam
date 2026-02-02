4b4355ceaa1477636812d68bf49cb81013dd22d8
JunwooPark <76034729+kodugi@users.noreply.github.com>
2026-02-02 09:20:11 +0900

    Enemy 로직 수정
    남은 선택 가능 횟수가 2회 이상이면 스킵 불가하게 만듦
    Players를 public으로 변경
    RevealAnswer 시 정오 여부가 Enemy의 _candidates에도 영향을 미치도록 변경
    _remainingChoices를 public으로 변경

--- Assets/Resources/Monoscripts/Enemy.cs
+++ Assets/Resources/Monoscripts/Enemy.cs
@@ -11,7 +11,8 @@ public class Enemy: Player
         // row가-1이면 skip을 의미
         var currentSentenceData = _gameManager.GetCurrentSentenceData();
         int currentColumn = _gameManager.GetCurrentColumn();
-        int randomIndex = UnityEngine.Random.Range(0, _wordCandidates.sentences[currentColumn].Count + 1);
+        int randomIndex = UnityEngine.Random.Range(0, _wordCandidates.sentences[currentColumn].Count + (_gameManager.GetRemainingChoices() >= 2 ? 0 : 1));
+        // 남은 선택 횟수가 2회 이상이면 skip 불가
         // 일단은 위치를 가능한 후보군 중 랜덤으로 선택
         if(randomIndex < _wordCandidates.sentences[currentColumn].Count)
         {
@@ -30,5 +31,15 @@ public class Enemy: Player
     public void RemoveAt(Position pos)
     {
         _wordCandidates.sentences[pos.col].RemoveAt(pos.row);
+    }
+
+    public void RemoveWord(WordData wordData, int column)
+    {
+        _wordCandidates.sentences[column].RemoveAll(wd => wd.word == wordData.word);
+    }
+
+    public void RemoveExceptAnswer(int column)
+    {
+        _wordCandidates.sentences[column].RemoveAll(wd => !wd.isCorrect);
     }
 }
