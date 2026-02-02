eedc054d56ef94f16fc45ef4a6dea477b261e61f
JunwooPark <76034729+kodugi@users.noreply.github.com>
2026-02-02 18:28:21 +0900

    튜토리얼 스테이지 틀 추가

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField] private Button nextButton;
+    [SerializeField] private TMPro.TextMeshProUGUI tutorialText;
+    [SerializeField] private Image tutorialImage;
+    private int currentStep = 0;
+
+    private TutorialData tutorialData;
+
+    private void Start()
+    {
+        TextAsset tutorialJson = Resources.Load<TextAsset>("TutorialData/TutorialDialogue");
+        tutorialData = JsonUtility.FromJson<TutorialData>(tutorialJson.text);
+
+        nextButton.onClick.AddListener(OnNextButtonClicked);
+
+        ShowStep(currentStep);
+    }
+
+    private void ShowStep(int stepIndex)
+    {
+        if (stepIndex < tutorialData.steps.Length)
+        {
+            tutorialText.text = tutorialData.steps[stepIndex].text;
+            tutorialImage.sprite = Resources.Load<Sprite>(tutorialData.steps[stepIndex].imagePath);
+        }
+        else
+        {
+            // End of tutorial, maybe hide the tutorial UI
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnNextButtonClicked()
+    {
+        currentStep++;
+        ShowStep(currentStep);
+    }
+}
